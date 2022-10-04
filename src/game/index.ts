65e545d99db38e00d11b6d685ddda30e5f49bd62
Ryan Brinkley <ryanbrinkley@yahoo.com>
2022-10-04 18:53:06 -0500

    Press a button to shuffle and deal 6
    That's all I got so far

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,16 +1,67 @@
 import type { Game, Move } from 'boardgame.io';
 import { GAME_ID } from '../config';
+import { battleCards } from './cards';
+import { CardInfo } from './cards';
 
 export interface GameState {
   // aka 'G', your game's state
+  players: Player[];
+  secret: SecretInfo;
+}
+export interface Player {
+  ID: string;
+  ready: boolean;
+  firstPlayer: boolean;
+  cards: CardInfo[];
+}
+export interface SecretInfo {
+  deck: CardInfo[];
+  discardPile: CardInfo[];
 }
 
-const move1: Move<GameState> = (G, ctx) => {};
-const move2: Move<GameState> = (G, ctx) => {};
+const selectCard: Move<GameState> = (G, ctx) => {};
+const placeCardFaceDown: Move<GameState> = (G, ctx) => {};
+const placeCardFaceUp: Move<GameState> = (G, ctx) => {};
+const resign: Move<GameState> = (G, ctx) => {};
+const shuffleAndDeal: Move<GameState> = (G, ctx) => {
+  G.secret.deck = G.secret.deck
+    .map((card) => ({ card, randNum: Math.random() }))
+    .sort((a, b) => a.randNum - b.randNum)
+    .map(({ card }) => card);
+  G.players[0].cards = G.secret.deck.splice(0, 6);
+  G.players[1].cards = G.secret.deck.splice(0, 6);
+};
 
 export const AirLandSea: Game<GameState> = {
   name: GAME_ID,
   minPlayers: 2,
   maxPlayers: 2,
-  moves: { move1, move2 },
+
+  setup: () => ({
+    secret: { deck: battleCards, discardPile: [] },
+    players: [
+      { ID: '0', firstPlayer: true, cards: [], ready: false },
+      { ID: '1', firstPlayer: false, cards: [], ready: false },
+    ],
+  }),
+
+  phases: {
+    shuffleAndDeal: {
+      start: true,
+      next: 'main',
+      moves: { shuffleAndDeal },
+    },
+    main: {
+      turn: {
+        order: {
+          first: (G, ctx) => Number(G.players[0].firstPlayer ? '0' : '1'),
+          next: (G, ctx) => Number(G.players[0].firstPlayer ? '1' : '0'),
+        },
+        stages: {
+          select: { moves: { selectCard, resign } },
+          place: { moves: { placeCardFaceDown, placeCardFaceUp } },
+        },
+      },
+    },
+  },
 };
