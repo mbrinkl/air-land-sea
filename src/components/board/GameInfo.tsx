895cb5757ae6fda28fdb93306c8420f4ce146706
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-10-04 21:16:19 -0500

    improve board, start connecting to game logic

--- src/components/board/GameInfo.tsx
+++ src/components/board/GameInfo.tsx
@@ -1,11 +1,21 @@
+import { useBoardContext } from './Board';
+
 export const GameInfo = (): JSX.Element => {
-  // get from gamestate...
-  // score for each player
+  const { G, playerID, moves, isActive } = useBoardContext();
+  const playerTurnOrder = G.players.find((p) => p.ID === playerID)?.firstPlayer
+    ? '1st'
+    : '2nd';
+
   return (
     <>
       <h6>Baddie score: 0</h6>
 
-      <h5>You are the nth player</h5>
+      <button onClick={() => moves.resign()} disabled={!isActive}>
+        Resign
+      </button>
+
+      <h5>You are the {playerTurnOrder} player</h5>
+      <h5>Your turn: {isActive ? 'YES' : 'NO'}</h5>
       <h5>Scoring info?</h5>
 
       <h6>My Score: 0</h6>
