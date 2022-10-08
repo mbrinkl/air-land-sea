1ec9d5b1ecce1965a6eab6b1a1845c46b5164ed5
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-10-07 22:28:27 -0500

    finish migrating from cra to next

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+// jest.config.js
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './src',
+});
+
+const config: Config = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+export default createJestConfig(config);
