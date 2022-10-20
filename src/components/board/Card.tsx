b189bf69c142820d1d29d923601a0e341910879a
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-10-19 23:42:55 -0500

    fix lint err

--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { colors } from 'config/theme';
 import { Card as GameCard } from 'game/cards';
 import { useBoardContext } from './Board';
