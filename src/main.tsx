d6aa6385b79ffa31b08655d3f1df94a4c3357c34
mbrinkl <bjbrinkleyyy@yahoo.com>
2023-01-04 16:24:59 -0600

    condense

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { store } from './store';
+import { theme } from './config/theme';
+import Home from './components/lobby/Home';
+import SocketGame from './components/lobby/SocketGame';
+import LocalGame from './components/lobby/LocalGame';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/local',
+    element: <LocalGame />,
+  },
+  {
+    path: '/:matchID',
+    element: <SocketGame />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 );
