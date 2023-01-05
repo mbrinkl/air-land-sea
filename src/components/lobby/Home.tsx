7ef0758d506c3d3feca801b5b3abd63925110a11
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2023-01-05 00:52:45 -0600

    lobby ui (#35)

--- src/components/lobby/Home.tsx
+++ src/components/lobby/Home.tsx
@@ -1,37 +1,50 @@
-import { Input, Button } from '@chakra-ui/react';
+import { Input, Button, Flex } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Home = (): JSX.Element => {
-  const navigate = useNavigate();
   const [joinMatchID, setJoinMatchID] = useState('');
+  const navigate = useNavigate();
 
   const onHostClicked = () => {
     const matchID = (Math.random() + 1).toString(36).substring(7);
     navigate(`/${matchID}?host`);
   };
 
-  const onJoinClicked = () => {
-    navigate(`/${joinMatchID}`);
-  };
-
-  const onLocalClicked = () => {
-    navigate('/local');
-  };
-
   return (
-    <div>
+    <Flex
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+      gap="15px"
+    >
       <Input
+        maxW="400px"
+        size="lg"
         value={joinMatchID}
         placeholder="Match ID to Join"
         onChange={(e) => setJoinMatchID(e.target.value)}
       />
-      <Button onClick={onHostClicked}>Host</Button>
-      <Button onClick={onJoinClicked} disabled={joinMatchID.length === 0}>
-        Join
-      </Button>
-      <Button onClick={onLocalClicked}>Local</Button>
-    </div>
+      <Flex gap="15px">
+        <Button onClick={onHostClicked} size="lg" colorScheme="green">
+          Host
+        </Button>
+        <Button
+          onClick={() => navigate(joinMatchID)}
+          disabled={joinMatchID.length === 0}
+          size="lg"
+          colorScheme="blue"
+        >
+          Join
+        </Button>
+        {import.meta.env.DEV && (
+          <Button onClick={() => navigate('local')} size="lg">
+            Local
+          </Button>
+        )}
+      </Flex>
+    </Flex>
   );
 };
 
