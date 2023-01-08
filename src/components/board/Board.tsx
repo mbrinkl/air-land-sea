01533f9506be321aaa115b7b41a52a2da0932023
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2023-01-07 23:28:17 -0600

    setup ui testing (#46)

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,7 +1,6 @@
 import { BoardProps } from 'boardgame.io/react';
-import { Box, VStack, Flex, Text } from '@chakra-ui/react';
+import { Box, VStack, Flex } from '@chakra-ui/react';
 import { GameState } from '../../game/gameTypes';
-import { useBoardStore } from '../../hooks/useBoardStore';
 import { BoardContext } from '../../hooks/useBoardContext';
 import Hand from './Hand';
 import TheaterRow from './TheaterRow';
@@ -11,7 +10,6 @@ import PlayerInfo from './PlayerInfo';
 import InvitePlayerButton from './InvitePlayerButton';
 
 const Board = (boardProps: BoardProps<GameState>): JSX.Element => {
-  const hoveredCardInfo = useBoardStore((s) => s.hoveredCardInfo);
   const opponentID = (Number(boardProps.playerID) ^ 1).toString();
   const isLocalGame = boardProps.matchData![0].isConnected === undefined;
   const has2ndPlayerJoined = boardProps.matchData![1].isConnected !== undefined;
@@ -45,12 +43,7 @@ const Board = (boardProps: BoardProps<GameState>): JSX.Element => {
           <Box px="10px">
             <PlayerInfo playerID={boardProps.playerID!} />
           </Box>
-          <Box textAlign="center">
-            <HelpText />
-            <Text>
-              {hoveredCardInfo.length > 0 ? hoveredCardInfo : '\u00A0'}
-            </Text>
-          </Box>
+          <HelpText />
           <Hand type="self" />
           <Flex alignItems="center" justifyContent="center" gap="3px">
             <Controls />
--- src/testUtil/index.ts
+++ src/testUtil/index.ts
@@ -1,7 +1,50 @@
 import { Local } from 'boardgame.io/multiplayer';
 import { AirLandSea } from '../game';
+import { BoardProps } from 'boardgame.io/react';
+import { GameState } from '../game/gameTypes';
 
 export const createGame = () => ({
   game: AirLandSea,
   multiplayer: Local(),
+});
+
+export const createMockBoardProps = (): BoardProps<GameState> => ({
+  G: {
+    ...AirLandSea.setup!(
+      {} as Parameters<NonNullable<(typeof AirLandSea)['setup']>>[0],
+    ),
+  },
+  ctx: {
+    numPlayers: 1,
+    playOrder: ['0'],
+    activePlayers: null,
+    playOrderPos: 0,
+    turn: 0,
+    phase: '',
+    currentPlayer: '0',
+  },
+  moves: {
+    selectCard: vi.fn(),
+    improvise: vi.fn(),
+    deploy: vi.fn(),
+    withdraw: vi.fn(),
+  },
+  matchData: [{ id: 0 }, { id: 1 }],
+  events: {},
+  plugins: {},
+  playerID: '0',
+  isActive: true,
+  isConnected: true,
+  isMultiplayer: true,
+  matchID: 'default',
+  log: [],
+  deltalog: [],
+  chatMessages: [],
+  sendChatMessage: vi.fn(),
+  redo: vi.fn(),
+  undo: vi.fn(),
+  reset: vi.fn(),
+  _stateID: 0,
+  _redo: [],
+  _undo: [],
 });
